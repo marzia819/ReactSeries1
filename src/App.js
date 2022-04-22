d714340c40c30131d2f24d4f1340f11564f8e29c
alizada-hadi <alizadahadi2050@gmail.com>
2022-04-22 16:53:14 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <nav>
+        <img src={logo} width="100px" />
+      </nav>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>It's composable</li>
+        <li>It's declarative</li>
+        <li>It's very powerful</li>
+        <li>It's developed by Meta</li>
+        <li>There are many skillful developer behined creating it</li>
+      </ul>
     </div>
   );
 }
