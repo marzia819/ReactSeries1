af76661e02bc6fff120e6e5d5e5237bfad3699c7
alizada-hadi <alizadahadi2050@gmail.com>
2022-04-22 17:21:47 +0430

    changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./components/navbar";
+import Content from './components/content';
 function App() {
   return (
     <div className="">
-      <nav>
-        <img src={logo} width="100px" />
-      </nav>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>It's composable</li>
-        <li>It's declarative</li>
-        <li>It's very powerful</li>
-        <li>It's developed by Meta</li>
-        <li>There are many skillful developer behined creating it</li>
-      </ul>
+      <Navbar />
+      <Content />
     </div>
   );
 }
---
+++ src/components/content.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Content = () => {
+    return (
+        <div className="container mx-auto">
+            <h1 className=" text-6xl text-dark-700 mt-20">Fun facts about React</h1>
+            <ul className="list-disc ml-10 text-2xl mt-2">
+                <li className="my-1">React powered by Meta</li>
+                <li className="my-1">React has a component based structure</li>
+                <li className="my-1">React is competable with everything</li>
+                <li className="my-1">React is a declaritive</li>
+            </ul>
+        </div>
+    )
+}
+export default Content
